b95ac8c34309129dff42e192a68895105b0de3b1
Mojatto <daichiandroid1997+work@gmail.com>
2022-04-22 01:35:50 +0900

    ADD:nativeshell, oauth1パッケージを追加。

---
+++ lib/Config/definitions.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/cupertino.dart';
+
+// TwitterAuthorizeWindow Definitions
+const String twitterAuthWindowTitle = 'Twitter Authorization';
+const Size twitterAuthWindowSize = Size(300, 200);
