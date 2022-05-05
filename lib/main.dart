754473f7b46a48156a2bd1cf967613bc39ce9127
Mojatto <daichiandroid1997+work@gmail.com>
2022-05-06 03:44:48 +0900

    ADD:サンプルウィジェットの定義を追加．

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:html';
 import 'dart:io' show Platform;
 
 import 'package:flutter/material.dart';
@@ -20,10 +21,63 @@ class CommonWindow extends StatelessWidget {
   }
 }
 
+class Choice {
+  const Choice({required this.title});
+
+  final String title;
+}
+
+const List<Choice> choices = const <Choice>[
+  const Choice(title: 'TWEET'),
+  const Choice(title: 'CONFIG'),
+];
+
 class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return MaterialApp(
+      home: DefaultTabController(
+        length: choices.length,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Bulletter'),
+            bottom: TabBar(
+              tabs: choices.map((Choice choice) {
+                return Tab(
+                  text: choice.title,
+                );
+              }).toList(),
+            ),
+            actions: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ChoiceCard extends StatelessWidget {
+  const ChoiceCard({required Key key, required this.choice}) : super(key: key);
+
+  final Choice choice;
+
+  @override
+  Widget build(BuildContext context) {
+    final TextStyle? textStyle = Theme.of(context).textTheme.displayLarge;
+
+    return Card(
+      color: Colors.white,
+      child: Center(
+          child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            choice.title,
+            style: textStyle,
+          ),
+        ],
+      )),
+    );
   }
 }
