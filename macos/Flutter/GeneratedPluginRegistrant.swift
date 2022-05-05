2665a850c981aca0434de9a40ad8b2e4a0e21cb2
Mojatto-MBP <daichiandroid1997@gmail.com>
2022-05-06 05:29:31 +0900

    ADD:macOS用ランナーコードを追加．

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,18 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import bitsdojo_window_macos
+import desktop_multi_window
+import eyro_toast
+import system_tray
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  BitsdojoWindowPlugin.register(with: registry.registrar(forPlugin: "BitsdojoWindowPlugin"))
+  FlutterMultiWindowPlugin.register(with: registry.registrar(forPlugin: "FlutterMultiWindowPlugin"))
+  EyroToastPlugin.register(with: registry.registrar(forPlugin: "EyroToastPlugin"))
+  SystemTrayPlugin.register(with: registry.registrar(forPlugin: "SystemTrayPlugin"))
+}
