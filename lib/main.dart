07034ff3c7a27a02a76227aea87e0a93bcda7109
Mojatto <daichiandroid1997+work@gmail.com>
2022-05-06 05:07:10 +0900

    ADD:起動時にアプリを表示化しシステムトレイに格納する処理を追加．

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
-import 'dart:html';
-import 'dart:io' show Platform;
+import 'dart:io';
 
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/material.dart';
 
 import 'package:bulletter/UI/config_interface.dart' as configUi;
+import 'package:bulletter/Config/definitions.dart' as definitions;
+import 'package:system_tray/system_tray.dart';
 
 void main() {
+  // Flutter アプリ本体前に Flutter エンジンを用いる処理を実行できるように設定
+  WidgetsFlutterBinding.ensureInitialized();
+  // Flutter アプリ本体を実行
   runApp(MainApp());
+
+  doWhenWindowReady(() {
+    final win = appWindow;
+    win.minSize = win.size = definitions.initialSize;
+    win.alignment = Alignment.center;
+    win.title = definitions.appTitle;
+  });
 }
 
 class CommonWindow extends StatelessWidget {
@@ -32,32 +44,8 @@ const List<Choice> choices = const <Choice>[
   const Choice(title: 'CONFIG'),
 ];
 
-class MainApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: DefaultTabController(
-        length: choices.length,
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text('Bulletter'),
-            bottom: TabBar(
-              tabs: choices.map((Choice choice) {
-                return Tab(
-                  text: choice.title,
-                );
-              }).toList(),
-            ),
-            actions: [],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
 class ChoiceCard extends StatelessWidget {
-  const ChoiceCard({required Key key, required this.choice}) : super(key: key);
+  const ChoiceCard({Key? key, required this.choice}) : super(key: key);
 
   final Choice choice;
 
@@ -78,6 +66,90 @@ class ChoiceCard extends StatelessWidget {
           ),
         ],
       )),
+    );
+  }
+}
+
+class MainApp extends StatelessWidget {
+  MainApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: definitions.appTitle,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MainAppPage(
+        title: '',
+      ),
+    );
+  }
+}
+
+class MainAppPage extends StatefulWidget {
+  const MainAppPage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<StatefulWidget> createState() => MainAppState();
+}
+
+class MainAppState extends State<MainAppPage> {
+  // システムトレイのインスタンス
+  final SystemTray systemTray = SystemTray();
+  // アプリ本体ウィンドウのインスタンス
+  final AppWindow appWindow = AppWindow();
+
+  Future<void> initSystemTray() async {
+    await systemTray.initSystemTray(
+      title: 'Bulletter',
+      iconPath: Platform.isWindows
+          ? definitions.appIconPath
+          : definitions.appImagePath,
+      toolTip: "Bulletter",
+    );
+
+    systemTray.registerSystemTrayEventHandler((eventName) {
+      if (eventName == "leftMouseDown") {
+      } else if (eventName == "leftMouseUp") {
+        appWindow.show();
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    initSystemTray();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: choices.length,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Bulletter'),
+            bottom: TabBar(
+              tabs: choices.map((Choice choice) {
+                return Tab(
+                  text: choice.title,
+                );
+              }).toList(),
+            ),
+          ),
+          body: TabBarView(
+            children: choices.map((Choice choice) {
+              return Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ChoiceCard(choice: choice),
+              );
+            }).toList(),
+          ),
+        ),
+      ),
     );
   }
 }
